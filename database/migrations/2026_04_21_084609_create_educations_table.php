4dcd0586d6e4538754ae3756506266f9106b6fd3
phoenix-0sX <lanjarl054@gmail.com>
2026-04-21 12:25:32 +0300

    feat: implementing all initial migrations

---
+++ database/migrations/2026_04_21_084609_create_educations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('educations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('description')->nullable();
+            $table->string('graduation_title')->nullable();
+            $table->string('graduation_place')->nullable();
+            $table->integer('graduation_month')->nullable();
+            $table->integer('graduation_year')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('educations');
+    }
+};
---
+++ database/migrations/2026_04_21_090000_create_pictures_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained('projects')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('filename');
+            $table->string('hash_id');
+            $table->integer('type')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pictures');
+    }
+};
